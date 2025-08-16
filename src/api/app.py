3d54ce9a062705c5152793b7c64fdb33e0732109
RuslanChukavin <r66cha@gmail.com>
2025-08-16 23:20:41 +0300

    remove _0_main_router

--- src/api/app.py
+++ src/api/app.py
@@ -6,7 +6,6 @@ import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.core.config import settings
-from src.api.routers._0_main_router import main_router
 
 from src.api.routers.WebSocket import websocket_router
 from fastapi.responses import ORJSONResponse
@@ -33,7 +32,6 @@ def getUP() -> FastAPI:
     )
 
     # Routers
-    app.include_router(main_router)
     app.include_router(websocket_router)
 
     # Middleware
